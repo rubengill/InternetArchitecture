fe4658aedf32785edde75c736c25bfebb6b58e06
Ruben Gill <rgill288@my.bcit.ca>
2024-09-06 17:49:21 -0700

    Added code to generate the button, and randomize the colors.

---
+++ Lab1/js/button.js
@@ -0,0 +1,49 @@
+class Button {
+  constructor(width = "10em", height = "5em", label = null) {
+    this.width = width;
+    this.height = height;
+    this.label = label;
+  };
+
+  // Create the Button
+  createButtonElement() {
+    console.log("Creating Button!");
+    
+    const button = document.createElement('button');
+    button.textContent = this.label;
+    button.style.width = this.width;
+    button.style.height = this.height;
+    button.style.background = this.randomColor();
+
+    return button;
+  };
+
+  // Create the correct number of buttons
+  createButtons(n) {
+    const buttonArray = [];
+    for (let i = 0; i < n; i++) {
+      console.log("Creating Button!");
+      const button = this.createButtonElement();
+      buttonArray.push(button);
+    };
+
+    return buttonArray;
+  };
+
+  // Assign a random color to the button
+  randomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    };
+
+    return color;
+  };
+
+  //Append buttons to the DOM
+  appendButtons(button) {
+
+  };
+};
+
