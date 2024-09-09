fbcb9152b6d4239fac896e95b670b3a1a078568b
Ruben Gill <rgill288@my.bcit.ca>
2024-09-09 00:29:40 -0700

    Edited folder structure

---
+++ labs/0/js/button.js
@@ -0,0 +1,45 @@
+import messages from '../lang/messages/en/user.js'
+
+export class Button {
+  static defaultWidth = messages.width;
+  static defaultHeight = messages.height;
+
+  constructor(width = Button.defaultWidth, height = Button.defaultHeight, label) {
+    this.width = width;
+    this.height = height;
+    this.label = label;
+    this.backgroundColor = this.randomColor();
+    this.buttonElement = null;
+  };
+
+  createButtonElement() {
+    const button = document.createElement("div");
+    button.classList.add("square-button")
+
+    button.textContent = this.label;
+    button.style.backgroundColor = this.backgroundColor;
+    button.style.width = this.width;
+    button.style.height = this.height;
+    this.buttonElement = button;
+
+    return button;
+  };
+
+  randomColor() {
+    const letters = messages.letters;
+    let color = messages.color;
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * letters.length)];
+    };
+
+    return color;
+  };
+
+  hideButtonLabel() {
+    if (this.buttonElement) {
+      this.buttonElement.textContent = '';
+    };
+  };
+};
+
+
---
+++ labs/0/lang/messages/en/user.js
@@ -0,0 +1,12 @@
+const messages = {
+  width: "10em",
+  height: "5em",
+  letters: "456789ABCDEF",
+  color: "#",
+  alert: "Please enter a number between 3 and 7.",
+  congrats: "Congratulations! You clicked all the buttons in the correct order.",
+  error: "Error! Wrong button clicked. Revealing all button labels"
+
+}
+
+export default messages;
